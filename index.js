c17c941ccfdad36ad52c2a939ec1d1e09c6fb604
Guilhermepr0jetos <joseguilhermemmatos@gmail.com>
2024-08-04 15:20:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 const regex = /\[([^\]]+)\]/g;
 
 function parseArgs(messageContent) {
@@ -139,11 +147,3 @@ client.on('messageCreate', async message => {
 });
 
 client.login(process.env.token);
-
-app.get('/', (req, res) => {
-  res.send('Bot is running!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
